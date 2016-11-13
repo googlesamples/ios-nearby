257e69ebf5b0a554f119cde9ac97724ec49157f0
dan-webb <dwebb@google.com>
2016-11-13 12:21:22 -0800

    Responded to Swift code review comments, and updated Podfile syntax.

--- NearbyMessagesExample/NearbyMessagesExampleSwift/AppDelegate.swift
+++ NearbyMessagesExample/NearbyMessagesExampleSwift/AppDelegate.swift
@@ -55,7 +55,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     nearbyPermission = GNSPermission(changedHandler: {[unowned self] granted in
       self.messageViewController.leftBarButton =
         UIBarButtonItem(title: String(format: "%@ Nearby", granted ? "Deny" : "Allow"),
-          style: UIBarButtonItemStyle.plain, target: self, action: #selector(AppDelegate.toggleNearbyPermission))
+          style: .plain, target: self, action: #selector(AppDelegate.toggleNearbyPermission))
       })
     setupStartStopButton()
 
@@ -66,20 +66,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // published by nearby devices.
     messageMgr = GNSMessageManager(apiKey: kMyAPIKey,
       paramsBlock: {(params: GNSMessageManagerParams?) -> Void in
+        guard let params = params else { return }
+
         // This is called when microphone permission is enabled or disabled by the user.
-        params!.microphonePermissionErrorHandler = { hasError in
+        params.microphonePermissionErrorHandler = { hasError in
           if (hasError) {
             print("Nearby works better if microphone use is allowed")
           }
         }
         // This is called when Bluetooth permission is enabled or disabled by the user.
-        params!.bluetoothPermissionErrorHandler = { hasError in
+        params.bluetoothPermissionErrorHandler = { hasError in
           if (hasError) {
             print("Nearby works better if Bluetooth use is allowed")
           }
         }
         // This is called when Bluetooth is powered on or off by the user.
-        params!.bluetoothPowerErrorHandler = { hasError in
+        params.bluetoothPowerErrorHandler = { hasError in
           if (hasError) {
             print("Nearby works better if Bluetooth is turned on")
           }
@@ -93,13 +95,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func setupStartStopButton() {
     let isSharing = (publication != nil)
     messageViewController.rightBarButton = UIBarButtonItem(title: isSharing ? "Stop" : "Start",
-      style: UIBarButtonItemStyle.plain,
-      target: self, action: isSharing ? #selector(AppDelegate.stopSharing) :  #selector(AppDelegate.startSharing))
+      style: .plain,
+      target: self, action: isSharing ? #selector(AppDelegate.stopSharing) :  #selector(AppDelegate.startSharingWithRandomName))
   }
 
   /// Starts sharing with a randomized name.
-  func startSharing() {
-    startSharingWithName(String(format:"Anonymous %d", arc4random() % 100))
+  func startSharingWithRandomName() {
+    startSharing(withName: String(format:"Anonymous %d", arc4random() % 100))
     setupStartStopButton()
   }
 
@@ -118,7 +120,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   /// Starts publishing the specified name and scanning for nearby devices that are publishing
   /// their names.
-  func startSharingWithName(_ name: String) {
+  func startSharing(withName name: String) {
     if let messageMgr = self.messageMgr {
       // Show the name in the message view title and set up the Stop button.
       messageViewController.title = name
@@ -130,10 +132,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
       // Subscribe to messages from nearby devices and display them in the message view.
       subscription = messageMgr.subscription(messageFoundHandler: {[unowned self] (message: GNSMessage?) -> Void in
-        self.messageViewController.addMessage(String(data: message!.content, encoding:String.Encoding.utf8))
-        }, messageLostHandler: {[unowned self](message: GNSMessage?) -> Void in
-          self.messageViewController.removeMessage(String(data: message!.content, encoding: String.Encoding.utf8))
-        })
+        guard let message = message else { return }
+        self.messageViewController.addMessage(String(data: message.content, encoding:String.Encoding.utf8))
+      }, messageLostHandler: {[unowned self](message: GNSMessage?) -> Void in
+        guard let message = message else { return }
+        self.messageViewController.removeMessage(String(data: message.content, encoding: String.Encoding.utf8))
+      })
     }
   }
 }
